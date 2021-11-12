4957d96554a0f3940adc78a760f2e9bd4bc3cb6f
osu <joanosuna9@gmail.com>
2021-11-12 20:43:42 +0100

    Added registerRequestSchema

---
+++ database/requestSchemas/registerRequestSchema.js
@@ -0,0 +1,13 @@
+const { Joi } = require("express-validation");
+
+const userRegisterSchema = {
+  body: Joi.object({
+    name: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    age: Joi.number().required(),
+    image: Joi.string().optional(),
+  }),
+};
+
+module.exports = userRegisterSchema;
