df6d50f891200b8133f2480d9acec457c7f513ef
osu <joanosuna9@gmail.com>
2021-11-13 13:12:20 +0100

    Added loginUser function

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../database/models/user");
 
 const createUser = async (req, res, next) => {
@@ -19,4 +20,27 @@ const createUser = async (req, res, next) => {
   }
 };
 
-module.exports = { createUser };
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    const error = new Error("Wrong credentials");
+    error.code = 401;
+    next(error);
+  } else {
+    const rightPassword = await bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      const error = new Error("Wrong credentials");
+      error.code = 401;
+      next(error);
+    } else {
+      const token = jwt.sign(
+        { username, id: user.id },
+        process.env.SECRET_HASH
+      );
+      res.json({ token });
+    }
+  }
+};
+
+module.exports = { createUser, loginUser };
