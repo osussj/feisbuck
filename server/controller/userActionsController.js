15017eaf772dd4361833525c666fec011e32d312
osu <joanosuna9@gmail.com>
2021-11-13 23:17:52 +0100

    Added addFriend and getFriend function

--- server/controller/userActionsController.js
+++ server/controller/userActionsController.js
@@ -11,4 +11,34 @@ const getUsers = async (req, res, next) => {
   }
 };
 
-module.exports = { getUsers };
+const addFriend = async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const friend = await User.findById(id);
+    const user = await User.findOne({ username: req.userInfo.username });
+    user.friends = [...user.friends, friend.id];
+    await user.save(user);
+    res.json({ added: friend.id });
+  } catch {
+    const error = new Error("Friend not found");
+    error.code = 401;
+    next(error);
+  }
+};
+const getFriend = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      username: req.userInfo.username,
+    }).populate({
+      path: "friends",
+      select: "-password -id -friends -enemies -age",
+    });
+    res.json(user);
+  } catch {
+    const error = new Error("Error loading friends");
+    error.code = 500;
+    next(error);
+  }
+};
+
+module.exports = { getUsers, addFriend, getFriend };
