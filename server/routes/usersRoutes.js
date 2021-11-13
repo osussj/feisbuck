e32b5803b196520d07d475b3be924bfffac1cb3a
osu <joanosuna9@gmail.com>
2021-11-13 13:12:44 +0100

    Added login endpoint with loginUser

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const { validate } = require("express-validation");
+const userLoginSchema = require("../../database/requestSchemas/loginRequestSchema");
 
 const {
   userRegisterSchema,
 } = require("../../database/requestSchemas/registerRequestSchema");
 
-const { createUser } = require("../controller/userController");
+const { createUser, loginUser } = require("../controller/userController");
 
 const router = express.Router();
 
 router.post("/register", validate(userRegisterSchema), createUser);
+
+router.post("/login", validate(userLoginSchema), loginUser);
 
 module.exports = router;
