439c91a97170690eb4bb542bc37c2a60b2063908
osu <joanosuna9@gmail.com>
2021-11-13 23:17:13 +0100

    Added getFriend and addFriend to socialRoutes

--- server/routes/socialRoutes.js
+++ server/routes/socialRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const { getUsers } = require("../controller/userActionsController");
+const {
+  getUsers,
+  addFriend,
+  getFriend,
+} = require("../controller/userActionsController");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.get("/", auth, getUsers);
+
+router.get("/friends", auth, getFriend);
+
+router.post("/friends", auth, addFriend);
 
 module.exports = router;
